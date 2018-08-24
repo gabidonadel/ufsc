9cda97986a36b316cc85467894dc17e1ecefb759
Gabriel Dall'Agnol <gabidonadel1@gmail.com>
2018-08-24 15:23:31 -0300

    Aula 2

---
+++ src/aula2/Losango.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula2;
+
+/**
+ *
+ * @author 12041789921
+ */
+public class Losango extends Figura{
+    private int raio;
+
+    public int getRaio() {
+        return raio;
+    }
+
+    public void setRaio(int raio) {
+        this.raio = raio;
+    }
+
+    public Losango(int raio) {
+        this.raio = raio;
+    }
+    
+    public void desenhe(){
+        int NumEspacos = raio;
+        String simbulo = "█";
+        String linhas = "█  █";
+        //primeira metade
+        for(int i=1;i<=this.raio/2;i++){ 
+            String espacos="";
+            for(int j=1;j<=NumEspacos;j++){
+                espacos+=" ";
+            }
+            NumEspacos--;
+            System.out.print(espacos);
+            if(i==1){
+                System.out.println(" "+simbulo);
+            }else{
+                System.out.println(linhas);
+                linhas = "█"+"  "+linhas.substring(1);
+            }
+        }
+        //meio
+        if(raio%2 ==1){
+            String espacos=" ";
+            for(int j=1;j<=this.raio/2;j++){
+                espacos+=" ";
+            }
+            System.out.print(espacos);
+            System.out.println(linhas);
+            linhas = "█"+linhas.substring(3);
+        }
+        //segunda metade
+        for(int i=1;i<=this.raio/2;i++){ 
+            String espacos=" ";
+            for(int j=1;j<=NumEspacos;j++){
+                espacos+=" ";
+            }
+            NumEspacos++;
+            System.out.print(espacos);
+            if(i== this.raio/2){
+                System.out.println(" "+simbulo);
+            }else{
+                
+                System.out.println(linhas);
+                linhas = "█"+linhas.substring(3);
+            }
+        }
+        
+        //System.out.println(Plinha);
+    }
+    
+}
---
+++ src/aula2/Retangulo.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula2;
+
+/**
+ *
+ * @author 12041789921
+ */
+public class Retangulo extends Figura implements Armazenavel{
+    private int altura;
+    private int largura;
+
+    public Retangulo(int altura, int largura) {
+        this.altura = altura;
+        this.largura = largura;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public int getLargura() {
+        return largura;
+    }
+
+    public void setLargura(int largura) {
+        this.largura = largura;
+    }
+    public void desenhe(){
+        //primeira linha
+        String simbulo = "█";
+        String linha="";
+        System.out.println(primeiraLinha(simbulo, linha));
+        //meio
+        linha="";
+        linha+=simbulo;
+        for(int i=1;i<=largura-2;i++){
+            linha+="    ";
+        }
+        linha+=simbulo;
+        for(int i=2;i<altura;i++){
+            System.out.println(linha);
+        }
+        //ultima livra
+        linha="";
+        System.out.println(primeiraLinha(simbulo, linha));
+    }
+    public String primeiraLinha(String simbulo,String linha){
+        for(int i=1;i<=largura;i++){
+            linha+=simbulo+"  ";
+        }
+        return linha;
+    } 
+    
+    @Override
+    public void grava(){
+        System.out.println("Gravou no banco!");
+    }
+    @Override
+    public Armazenavel recupera(){
+        System.out.println("Recupera do Banco");
+        return this;
+    } 
+}
